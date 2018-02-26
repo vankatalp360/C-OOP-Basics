f8fb1acef71a56b21c3223b3442508208d358bba
vankatalp360 <ivan.dimitrov81@gmail.com>
2018-02-26 19:00:34 +0200

    add

---
+++ INHERITANCE/EXERCISES/06.Anumals/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Animal
+{
+    private string name;
+    private int age;
+    private string gender;
+
+    public virtual string Gender
+    {
+        get { return gender; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value) ||
+                (value != "Male" && value != "Female"))
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+            gender = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+            age = value;
+        }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+            name = value;
+        }
+    }
+
+
+    public virtual string ProduceSound()
+    {
+        return $"";
+    }
+
+    public Animal()
+    {
+        
+    }
+    public Animal(string name, int age, string gender)
+    {
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"{this.GetType()}");
+        builder.AppendLine($"{name} {age} {Gender}");
+        builder.AppendLine($"{ProduceSound()}");
+
+        return builder.ToString().TrimEnd();
+    }
+}
---
+++ INHERITANCE/EXERCISES/06.Anumals/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var animals = new List<Animal>();
+        string command;
+
+        while ((command = Console.ReadLine()) != "Beast!")
+        {
+            try
+            {
+                ReadAnimals(animals, command);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine(animal);
+        }
+    }
+
+    private static void ReadAnimals(List<Animal> animals, string command)
+    {
+        Animal animal = new Animal();
+        string animalType = "";
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 1)
+            {
+                command = Console.ReadLine();
+            }
+            var input = command.Split(new[] { ' ' }, StringSplitOptions.None);
+            if (input.Length == 1)
+            {
+                animalType = input[0];
+            }
+            else if (input.Length == 3)
+            {
+                var name = input[0];
+                var age = int.Parse(input[1]);
+                var gender = "";
+                if (input.Length == 3)
+                {
+                    gender = input[2];
+                }
+                switch (animalType)
+                {
+                    case "Cat":
+                        animal = new Cat(name, age, gender);
+                        break;
+                    case "Dog":
+                        animal = new Dog(name, age, gender);
+                        break;
+                    case "Frog":
+                        animal = new Frog(name, age, gender);
+                        break;
+                    case "Tomcat":
+                        animal = new Tomcat(name, age);
+                        break;
+                    case "Kitten":
+                        animal = new Kitten(name, age);
+                        break;
+                    default: throw new ArgumentException($"Invalid input!");
+                }
+                animals.Add(animal);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid input!");
+            }
+        }
+    }
+}
